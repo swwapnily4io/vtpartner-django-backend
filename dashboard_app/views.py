43280227ae5f476c5f34afa3822b529403afc670
VTPartner <vtpartner7@gmail.com>
2025-04-06 10:38:01 +0530

    s

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -112,7 +112,7 @@ def select_query(query, params=None):
 
             # if result == []:
             #     raise ValueError("No Data Found")  # Custom error when no results are found
-            # print("query result::",result)
+            print("query result::",result)
             return result
 
     except ValueError as e:
