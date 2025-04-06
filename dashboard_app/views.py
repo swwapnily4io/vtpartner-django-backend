ed2d591d6cc19d554a44031eb86b009e363ed223
VTPartner <vtpartner7@gmail.com>
2025-04-06 12:38:24 +0530

    s

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -9105,7 +9105,7 @@ def get_total_handyman_un_verified_with_count(request):
                         }
                 else:
                     handyman_data["service_details"] = None
-                print(f"handyman_data: {handyman_data}")
+            
                 mapped_results.append(handyman_data)
 
             return JsonResponse({
