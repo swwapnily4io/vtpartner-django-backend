25d0dc29a8f34d3b1b0c49e63edc96397b7d1e65
VTPartner <vtpartner7@gmail.com>
2025-04-06 12:41:15 +0530

    s

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -9085,8 +9085,6 @@ def get_total_handyman_un_verified_with_count(request):
                 # Add service details if available
                 if row[18] != -1:
                     try:
-                        print("row->", row)
-                        print("row[39]:", row[39])
                         handyman_data["service_details"] = {
                             "service_name": str(row[38]) if row[38] else "NA",  # Ensure string
                             "service_image": str(row[39]) if row[39] else "NA",  # Ensure string
