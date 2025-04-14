117d211eec32a4e01cd72e814c1c503f95655a20
VTPartner <vtpartner7@gmail.com>
2025-04-14 13:47:33 +0530

    s

--- mobile_app/views.py
+++ mobile_app/views.py
@@ -6960,13 +6960,13 @@ def generate_new_goods_drivers_booking_id_get_nearby_drivers_with_fcm_token(requ
                     payment_method, city_id,sender_name,sender_number,receiver_name,receiver_number,pickup_address,drop_address,
                     coupon_applied,coupon_id,coupon_amount,before_coupon_amount,
                     is_scheduled, scheduled_time, drop_locations, drop_contacts,
-                    multiple_drops, body_type
+                    multiple_drops, body_type,vehicle_price_type, vehicle_radius_km
                 ) 
                 VALUES (
                     %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
                     EXTRACT(EPOCH FROM CURRENT_TIMESTAMP), CURRENT_DATE,  %s, %s, %s, 
                     %s, %s,%s, %s,%s, %s,%s, %s,%s,%s,%s,%s,
-                    %s, %s, %s, %s, %s, %s
+                    %s, %s, %s, %s, %s, %s, %s, %s
                 ) 
                 RETURNING booking_id;
             """
@@ -6988,7 +6988,7 @@ def generate_new_goods_drivers_booking_id_get_nearby_drivers_with_fcm_token(requ
                 gst_amount, igst_amount, payment_method, city_id,sender_name,sender_number,receiver_name,receiver_number,pickup_address,drop_address,
                 coupon_applied,coupon_id,coupon_amount,before_coupon_amount,
                 is_scheduled, scheduled_time, drop_locations_json, drop_contacts_json,
-                multiple_drops, body_type
+                multiple_drops, body_type,price_type,5
             ]
 
             # Assuming insert_query is a function that runs the query
