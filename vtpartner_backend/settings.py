35a09766494f76fa07304fa33d190e931bca958a
Vff-Group <velvet.fierce2020@gmail.com>
2025-05-14 19:15:59 +0530

    s

--- vtpartner_backend/settings.py
+++ vtpartner_backend/settings.py
@@ -33,14 +33,7 @@ SECRET_KEY = 'django-insecure-%$w3*@00r2^t+e!q4sy-d_^!7)1j4axp=-(iigv9t*q5t*qxff
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'kaps9.in',
-    'www.kaps9.in',
-    'localhost',
-    '127.0.0.1',
-    '100.24.44.74',
-    '.kaps9.in',
-]
+ALLOWED_HOSTS = ['vtpartner.org','www.vtpartner.org','vtpartner.in','kaps9.in','www.kaps9.in','www.vtpartner.in','localhost','127.0.0.1','77.37.47.156','http://localhost:3004','http://100.24.44.74/','100.24.44.74','https://100.24.44.74/']
 
 
 print("Host header:", os.environ.get("HTTP_HOST"))
