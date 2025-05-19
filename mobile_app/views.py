bb36781f468d09d94b5e7d933595fd8208195f98
Vff-Group <velvet.fierce2020@gmail.com>
2025-05-19 19:40:11 +0530

    f

--- mobile_app/views.py
+++ mobile_app/views.py
@@ -1228,9 +1228,9 @@ def get_agent_app_firebase_access_token(request):
     try:
         # Create a service account credential dictionary
         # load_dotenv('/root/.env_vtpartner')
-        
-        authToken = request.POST.get('authToken')
-        customer_id = request.POST.get('customer_id')
+        data = json.loads(request.body)
+        authToken = data.get('authToken')
+        customer_id = data.get('customer_id')
         if not is_valid_customer_fcm_token(customer_id, authToken):
             return JsonResponse({"message": "Unauthorized - Invalid FCM Token"}, status=401)
         load_dotenv('/root/.env_vtpartner_agent')
