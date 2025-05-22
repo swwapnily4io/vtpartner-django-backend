556d3e6e2d364c3056cb89efb6245f5eef9b406d
Vff-Group <velvet.fierce2020@gmail.com>
2025-05-22 11:40:07 +0530

    d

--- mobile_app/views.py
+++ mobile_app/views.py
@@ -14563,7 +14563,8 @@ def cab_booking_details_live_track(request):
             
         try:
             query = """
-                select booking_id,cab_bookings_tbl.customer_id,cab_bookings_tbl.driver_id,pickup_lat,pickup_lng,destination_lat,destination_lng,distance,cab_bookings_tbl.time,total_price,base_price,booking_timing,booking_date,booking_status,driver_arrival_time,otp,gst_amount,igst_amount,payment_method,cab_bookings_tbl.city_id,cancelled_reason,cancel_time,order_id,driver_first_name,cab_driverstbl.authtoken,customer_name,customers_tbl.authtoken,pickup_address,drop_address,customers_tbl.mobile_no,cab_driverstbl.mobile_no,vehiclestbl.vehicle_id,vehiclestbl.vehicle_name,vehiclestbl.image,vehicle_plate_no,vehicle_fuel_type,cab_driverstbl.profile_pic,coupon_applied,coupon_id,coupon_amount,before_coupon_amount,cab_bookings_tbl.hike_price from vtpartner.vehiclestbl,vtpartner.cab_bookings_tbl,vtpartner.cab_driverstbl,vtpartner.customers_tbl where cab_driverstbl.cab_driver_id=cab_bookings_tbl.driver_id and customers_tbl.customer_id=cab_bookings_tbl.customer_id and booking_id=%s and booking_status!='End Trip' and vehiclestbl.vehicle_id=cab_driverstbl.vehicle_id
+                select booking_id,cab_bookings_tbl.customer_id,cab_bookings_tbl.driver_id,pickup_lat,pickup_lng,destination_lat,destination_lng,distance,cab_bookings_tbl.time,total_price,base_price,booking_timing,booking_date,booking_status,driver_arrival_time,otp,gst_amount,igst_amount,payment_method,cab_bookings_tbl.city_id,cancelled_reason,cancel_time,order_id,driver_first_name,cab_driverstbl.authtoken,customer_name,customers_tbl.authtoken,pickup_address,drop_address,customers_tbl.mobile_no,cab_driverstbl.mobile_no,vehiclestbl.vehicle_id,vehiclestbl.vehicle_name,vehiclestbl.image,vehicle_plate_no,vehicle_fuel_type,cab_driverstbl.profile_pic,coupon_applied,coupon_id,coupon_amount,before_coupon_amount,
+                cab_bookings_tbl.hike_price,vehiclestbl.vehicle_map_image from vtpartner.vehiclestbl,vtpartner.cab_bookings_tbl,vtpartner.cab_driverstbl,vtpartner.customers_tbl where cab_driverstbl.cab_driver_id=cab_bookings_tbl.driver_id and customers_tbl.customer_id=cab_bookings_tbl.customer_id and booking_id=%s and booking_status!='End Trip' and vehiclestbl.vehicle_id=cab_driverstbl.vehicle_id
             """
             result = select_query(query,[booking_id])  # Assuming select_query is defined elsewhere
 
@@ -14615,6 +14616,7 @@ def cab_booking_details_live_track(request):
                     "coupon_amount":row[39],
                     "before_coupon_amount":row[40],
                     "hike_price":row[41],
+                    "vehicle_map_image":row[42],
                     
                 }
                 for row in result
@@ -14684,7 +14686,8 @@ def jcb_crane_driver_booking_details_live_track(request):
                 jcb_crane_driverstbl.mobile_no AS driver_mobile_no,
                 sub_cat_name,
                 service_name,
-                jcb_crane_driverstbl.profile_pic,coupon_applied,coupon_id,coupon_amount,before_coupon_amount,penalty_amount,penalty_charges_amount,jcb_crane_bookings_tbl.hike_price
+                jcb_crane_driverstbl.profile_pic,coupon_applied,coupon_id,coupon_amount,before_coupon_amount,penalty_amount,penalty_charges_amount,
+                jcb_crane_bookings_tbl.hike_price
             FROM 
                 vtpartner.jcb_crane_bookings_tbl
             LEFT JOIN 
