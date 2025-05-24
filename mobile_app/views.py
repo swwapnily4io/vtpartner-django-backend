64157465f82ff2b7b840f13c7beb103b148bdfc3
Vff-Group <velvet.fierce2020@gmail.com>
2025-05-24 12:00:14 +0530

    banners

--- mobile_app/views.py
+++ mobile_app/views.py
@@ -12092,11 +12092,11 @@ def get_all_banners(request):
     if request.method == "POST":
         
         try:
-            data = json.loads(request.body)
-            customer_id = data.get('customer_id')
-            authToken = data.get('auth')
-            if not is_valid_customer_fcm_token(customer_id, authToken):
-                return JsonResponse({"message": "Invalid or expired token. Please login again.","status":"unauthorized"}, status=401)
+            # data = json.loads(request.body)
+            # customer_id = data.get('customer_id')
+            # authToken = data.get('auth')
+            # if not is_valid_customer_fcm_token(customer_id, authToken):
+            #     return JsonResponse({"message": "Invalid or expired token. Please login again.","status":"unauthorized"}, status=401)
             
             query = """
                 SELECT banner_id, banner_title, banner_description, banner_image, 
