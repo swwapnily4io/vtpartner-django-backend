4ec2838bf2cc977a2e4f626f7ce2ef62dfd19f33
Vff-Group <velvet.fierce2020@gmail.com>
2025-06-10 08:23:40 +0530

    details ios

--- mobile_app/views.py
+++ mobile_app/views.py
@@ -9153,8 +9153,8 @@ def get_customer_details(request):
             data = json.loads(request.body)
             customer_id = data.get('customer_id')
             authToken = data.get('auth')
-            if not is_valid_customer_fcm_token(customer_id, authToken):
-                    return JsonResponse({"message": "Invalid or expired token. Please login again.","status":"unauthorized"}, status=401)
+            # if not is_valid_customer_fcm_token(customer_id, authToken):
+            #         return JsonResponse({"message": "Invalid or expired token. Please login again.","status":"unauthorized"}, status=401)
 
             if not customer_id:
                 return JsonResponse({"message": "Customer ID is required"}, status=400)
