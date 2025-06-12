733f61808204a9b42eaaef4c696d6c729ba2ce34
Vff-Group <velvet.fierce2020@gmail.com>
2025-06-12 15:30:07 +0530

    s

--- mobile_app/views.py
+++ mobile_app/views.py
@@ -279,13 +279,17 @@ def find_nearby_goods_drivers(booking):
                         )) <= %s
                     AND goods_driverstbl.category_id = vehiclestbl.category_id
                     AND goods_driverstbl.category_id = '1' AND  goods_driverstbl.vehicle_id=%s
-                    AND goods_driverstbl.body_type = %s
+                    AND (
+                        CASE 
+                            WHEN %s = 'Any' THEN goods_driverstbl.body_type IN ('Any', 'Open Body', 'Close Body')
+                            ELSE goods_driverstbl.body_type = %s
+                        END
+                    )
                     AND (goods_driverstbl.location_preference=%s OR goods_driverstbl.location_preference='0')
                     ORDER BY distance;
-
                     """
     values = [pickup_lat, pickup_lng, pickup_lat,city_id,vehicle_price_type, pickup_lat, pickup_lng, pickup_lat,vehicle_radius_km ,goods_vehicle_id,
-                              body_type,booking_type_locations]
+                              body_type,body_type,booking_type_locations]
     nearby_drivers = select_query(query, values)
 
     # Prepare FCM data
