cc645f2a1f489f1cb2e4bb786e98f93407a6de52
Vff-Group <velvet.fierce2020@gmail.com>
2025-06-15 15:54:25 +0530

    get_customer_auth_token

--- mobile_app/urls.py
+++ mobile_app/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
    path('custom_endpoint/', views.CustomEndpointView.as_view(), name='custom-endpoint'),
-   path('all_services_api/', views.all_services_api.as_view(), name='all_services_api')
+   path('get_customer_auth_token/', views.all_services_api.as_view(), name='get_customer_auth_token')
 ]
 
 # urlpatterns = [
