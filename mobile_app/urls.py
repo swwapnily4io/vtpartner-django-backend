82e978a1f587b0d8b16cfff6c3732f5c4fa6df25
Vff-Group <velvet.fierce2020@gmail.com>
2025-07-01 17:27:36 +0530

    s

--- mobile_app/urls.py
+++ mobile_app/urls.py
@@ -45,7 +45,7 @@ urlpatterns = [
     #Validate JWT Token
     path('validate-token', customer_login.ValidateCustomerTokenView.as_view(), name='validate_customer_token'),
     #Validate JWT Token Refresh
-    path('validate-token', customer_login.RefreshCustomerTokenView.as_view(), name='validate_customer_token'),
+    path('refresh-token', customer_login.RefreshCustomerTokenView.as_view(), name='validate_customer_token'),
     #Customer Login
     path('login',customer_login.login_view,name='login'),
     #Update Customer Firebase Token
