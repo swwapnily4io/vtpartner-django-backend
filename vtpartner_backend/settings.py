7efbc0e0f2a7d465f5b703a333b24234fc3bfe31
Vff-Group <velvet.fierce2020@gmail.com>
2025-07-08 10:05:51 +0530

    increased connections

--- vtpartner_backend/settings.py
+++ vtpartner_backend/settings.py
@@ -193,7 +193,12 @@ DATABASES = {
         'USER': 'mysuperuser',
         'PASSWORD': 'Vtpartner786',
         'HOST': 'kaps-database-1.c27aay0ieaae.us-east-1.rds.amazonaws.com',
-        'PORT': '5432'
+        'PORT': '5432',
+        'CONN_MAX_AGE': 600,  # Connection pooling
+        'OPTIONS': {
+            'connect_timeout': 10,
+            'MAX_CONNS': 20,
+        }
         # 'CONN_MAX_AGE': 120,  # keeps DB connection alive for 60 seconds
     }
 }
